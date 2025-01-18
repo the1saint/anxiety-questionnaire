513758afcb6e28a1c6b4c9cb5684e6224f3ab7a8
the1saint <131870755+the1saint@users.noreply.github.com>
2025-01-18 22:33:02 +0200

    Create page.js

---
+++ page.js
@@ -0,0 +1,9 @@
+import AnxietyQuestionnaire from '../components/AnxietyQuestionnaire';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <AnxietyQuestionnaire />
+    </main>
+  );
+}
